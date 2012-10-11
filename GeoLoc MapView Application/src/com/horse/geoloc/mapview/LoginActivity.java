f954822f9d89010c01b2b23cfd690b757a2d2b8a
nagi <nagi@nagi-Bilgisayar>
2012-10-11 13:43:16 +0300

    Login control added

--- GeoLoc MapView Application/src/com/horse/geoloc/mapview/LoginActivity.java
+++ GeoLoc MapView Application/src/com/horse/geoloc/mapview/LoginActivity.java
@@ -1,11 +1,21 @@
 package com.horse.geoloc.mapview;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+	EditText username,password;
+	Button ok;
+	
+	String usernm="nagi";
+	String pss="adem";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,10 +23,33 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
        
         
-        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "Wilkommen", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "nagihan", Toast.LENGTH_SHORT).show();
-        
+        username=(EditText)findViewById(R.id.UserNameET);
+        password=(EditText)findViewById(R.id.PasswordET);
+        ok=(Button)findViewById(R.id.LoginBT);
+        ok.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				try{
+					if(username.getText().toString()==usernm && password.getText().toString()==pss)
+					{
+					   Toast.makeText(getApplicationContext(),"Correct!", Toast.LENGTH_SHORT).show();
+						
+					}
+					else
+					{
+						Toast.makeText(getApplicationContext(),"Uncorrect!", Toast.LENGTH_SHORT).show();
+					}
+					
+				}
+				catch(Exception e)
+				{
+					username.setText(e.toString());
+					
+				}
+
+				
+			}
+		});
         
     }
 
